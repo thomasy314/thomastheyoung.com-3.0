39e86e8ff96f295a7594ded48ac4cfcad1e00950
Thomas Young <thomasy314@gmail.com>
2024-07-22 22:17:01 -0700

    Adding link to cluster mush website to social icons

--- components/ui/socialmediaicons.tsx
+++ components/ui/socialmediaicons.tsx
@@ -1,4 +1,4 @@
-import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandInstagram, IconBrandLinkedin, IconMushroom } from "@tabler/icons-react";
 
 type SocialMediaIconsProps = {
     size?: number,
@@ -9,13 +9,16 @@ export default function SocialMediaIcons({size, className}: SocialMediaIconsProp
     return (
         <div className={`flex flex-row justify-center gap-x-2 w-full ${className}`}>
             <a href="https://www.linkedin.com/in/thomasy314/" target="_blank">
-                <IconBrandLinkedin size={size} />
-            </a>
-            <a href="https://www.instagram.com/clustermush/" target="_blank">
-                <IconBrandInstagram size={size} />
+                <IconBrandLinkedin aria-label="Thomas Young Linkedin" size={size} />
             </a>
             <a href="https://github.com/thomasy314" target="_blank">
-                <IconBrandGithub size={size} />
+                <IconBrandGithub aria-label="Thomas Young Github" size={size} />
+            </a>
+            <a href="https://www.instagram.com/clustermush/" target="_blank">
+                <IconBrandInstagram aria-label="Cluster Mush Instagram" size={size} />
+            </a>
+            <a href="https://www.clustermush.com/" target="_blank">
+                <IconMushroom aria-label="Cluster Mush Icon" size={size} />
             </a>
         </div>
     )
