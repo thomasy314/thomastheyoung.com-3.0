43ecf4cbfc4e40ba02913c6f32ad5667d7abc674
Thomas Young <thomasy314@gmail.com>
2024-07-22 17:43:49 -0700

    Making them mountains dance (removing parallax lag)

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import SocialMediaIcons from "@/components/ui/socialmediaicons";
 
 export default function Home() {
   return (
-    <ParallaxBackground>
+    <ParallaxBackground disable>
       <div className="flex flex-col justify-center items-center w-full h-full">
         <h1 className="font-display text-6xl sm:text-7xl md:text-8xl lg:text-9xl">
           Thomas Young
